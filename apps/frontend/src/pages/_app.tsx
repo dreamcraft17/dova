2d5ea554f05d1b3a416437f4f634548f33b49e6c
arthur-da-1205 <arthur.darwanto@gmail.com>
2026-07-28 14:23:27 +0700

    feat: add modal login for checkout if not login yet, add reausable modal

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -2,4 +2,18 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { AuthProvider } from '../context/AuthContext';
 import { CartProvider } from '../context/CartContext';
-export default function App({ Component, pageProps }: AppProps) { return <AuthProvider><CartProvider><Component {...pageProps} /></CartProvider></AuthProvider>; }
+import { ToastProvider } from '../context/ToastContext';
+import { Toast } from '../components/Toast';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ToastProvider>
+      <AuthProvider>
+        <CartProvider>
+          <Component {...pageProps} />
+          <Toast />
+        </CartProvider>
+      </AuthProvider>
+    </ToastProvider>
+  );
+}
